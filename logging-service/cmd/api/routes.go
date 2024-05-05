e43e82c0d06c531324a472ebf8234fe1d0b7cf67
TuahaAjaz <tuahaajaz2000@gmail.com>
2024-05-05 12:27:35 -0700

    Mongo Docker Setup

---
+++ logging-service/cmd/api/routes.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func (app *Config) routes() http.Handler {
+	mux := gin.Default()
+
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"http://*", "https://*"}
+	config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE"}
+	config.AllowHeaders = []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"}
+	config.ExposeHeaders = []string{"Link"}
+	config.AllowCredentials = true
+	config.MaxAge = 300
+
+	mux.Use(cors.Default())
+
+	mux.GET("/logs", app.WriteLog)
+
+	return mux
+}
